e3fa58daf2ce072dc9bb58e6fdbc35da2d4fd159
Irvin H <irvinhomem@gmail.com>
2016-09-06 21:27:31 +0200

    Loading JSON of all pcap files from a single feature_base file. JSON decoding to a python list now works.

--- FeatureValidatorJSON.py
+++ FeatureValidatorJSON.py
@@ -12,24 +12,19 @@ class FeatureValidatorJSON(object):
         # self.logger.setLevel(logging.WARNING)
         self.logger.debug("Testing debug message")
 
-    def recursive_read_json(self, feat_base_path):
-        with open(feat_base_path) as json_data_file:
-            try:
-                single_json_obj = json.load(json_data_file)
-                self.logger.debug("JSON Obj filename: %s" % single_json_obj['filename'])
-            except json.decoder.JSONDecodeError as err:
-                self.logger.debug('Error: %s' % (str(err)))
-                self.logger.debug('Error: MSG: %s | Line #: %s | Col #: %s' % (str(err.msg), str(err.lineno), str(err.colno)))
-
     def validate_JSON_documents(self, protoLabel, featureName):
         feature_base_path = str(os.getcwd() + '/' + 'feature_base/JSON/' + protoLabel + '/' +
                                 featureName + '/' + featureName + '.json')
         self.logger.debug('Feature Base path: %s' % feature_base_path)
 
-        self.recursive_read_json(feature_base_path)
-
-
-
+        with open(feature_base_path) as json_data_file:
+            try:
+                all_json_objs = json.load(json_data_file)
+                self.logger.debug("JSON Obj filename: %s" % all_json_objs[0]['filename'])
+                self.logger.debug("JSON Obj filename: %s" % all_json_objs[1]['filename'])
+            except json.decoder.JSONDecodeError as err:     # A sub-class of ValueError
+                self.logger.debug('Error: %s' % (str(err)))
+                self.logger.debug('Error: MSG: %s | Line #: %s | Col #: %s' % (str(err.msg), str(err.lineno), str(err.colno)))
 
 
 
